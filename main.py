be07a18a22fab8ff6c72b25d33bd92b93a898a7f
Gokul <gokulkrishna.gk32@gmail.com>
2023-06-14 23:59:36 +0530

    update

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ RZR_KEY_SECRET= os.environ.get('RZR_KEY_SECRET')
 RZR_KEY_ID=os.environ.get('RZR_KEY_ID')
 
 razorpay_client = razorpay.Client(auth=(RZR_KEY_ID, RZR_KEY_SECRET))
-razorpay_client.set_app_details({"title" : "RzrPayOrder API", "version" : "0.5.0"})
+razorpay_client.set_app_details({"title" : "RzrPayOrder", "version" : "0.5.0"})
 
 @app.get("/",tags=["Test"])
 async def test_response():
     return "Api Start"
 
 @app.post("/order")
-async def create_order(amount=int):
-    data = { "amount": amount*100, "currency": "INR"}
+async def create_order(amount:int):
+    data = { "amount": int(amount)*100, "currency": "INR"}
     payment = razorpay_client.order.create(data=data)
     order_id=payment["id"]
     return order_id
