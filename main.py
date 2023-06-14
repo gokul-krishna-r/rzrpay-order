a2cc3cc45335c020cae62601915933964ec7b14c
Gokul <gokulkrishna.gk32@gmail.com>
2023-06-14 23:33:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from fastapi import Body
 from fastapi.middleware.cors import CORSMiddleware
 import os,base64,io
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(
     title="Razor Pay API",
@@ -22,16 +25,17 @@ RZR_KEY_SECRET= os.environ.get('RZR_KEY_SECRET')
 RZR_KEY_ID=os.environ.get('RZR_KEY_ID')
 
 razorpay_client = razorpay.Client(auth=(RZR_KEY_ID, RZR_KEY_SECRET))
-razorpay_client.set_app_details({"title" : "IMA MSN", "version" : "0.5.0"})
+razorpay_client.set_app_details({"title" : "RzrPayOrder", "version" : "0.5.0"})
 
 @app.get("/",tags=["Test"])
 async def test_response():
     return "Api Start"
 
 @app.post("/order")
-async def create_order(amount=Body(title="amount")):
-    data = { "amount": amount, "currency": "INR"}
+async def create_order(amount=int):
+    data = { "amount": amount*100, "currency": "INR"}
     payment = razorpay_client.order.create(data=data)
-    return payment
+    order_id=payment["id"]
+    return order_id
 
 
